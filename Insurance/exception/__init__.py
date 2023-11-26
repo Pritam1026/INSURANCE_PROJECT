02e906616013c35c7c8f2aa0d969c20474d9a595
Pritam1026 <singhpritam983@gmail.com>
2023-11-26 13:08:00 +0530

    utils and config file updated

--- Insurance/exception/__init__.py
+++ Insurance/exception/__init__.py
@@ -8,7 +8,7 @@ class InsuranceException(Exception):
 
     def __init__(self,error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=InsuranceException.error_messgae_detail(error=error_message,error_detail=error_detail)
+        self.error_message=InsuranceException.error_message_detail(error=error_message,error_detail=error_detail)
 
     @staticmethod
     def error_message_detail(error:Exception,error_detail:sys)->str:
--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
-import os,sys
 from Insurance.utils import get_collection_as_dataframe
+import sys, os
 
-def test_logger_and_exception():
-    try:
-        logging.info('Starting the test logger and exception')
-        result=3/0
-        logging.info('Ending point of test_logger and exception')
-        
-    except Exception as e:
-        print(e)
-        logging.debug(str(e))
-        raise InsuranceException(e,sys)
-    
 
 if __name__=="__main__":
     try:
-        database_name="INSURANCE"
-        collection_name="INSURANCE_PROJECT"
-        #test_logger_and_exception()
-        get_collection_as_dataframe(database_name=database_name,collection_name=collection_name)
-        
+        get_collection_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
     except Exception as e:
         print(e)
